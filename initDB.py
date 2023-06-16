2a0dd5932870f81e8a09f529e0572fead115b773
aerostatic <ichigolu@aol.com>
2023-06-16 01:17:38 +0000

    Added initial comments

--- initDB.py
+++ initDB.py
@@ -1,19 +1,47 @@
 import MySQLdb
 
+"""
+Database class. Supports simple functions regarding adding and deleting from table.
+"""
 class myDB:
     db = MySQLdb.connect(host="aerostatic.mysql.pythonanywhere-services.com",user="aerostatic",password="mypassword",database="aerostatic$TemperatureDB")
 
+    """
+    Ctor
+    """
     def __init__(self):
         pass
 
+    """
+    Dtor. Used for closing DB connection.
+    """
     def __del__(self):
         self.db.close()
 
+    """
+    Used to add temperature to the database.
+
+    Args:
+        time: Datetime string
+        temp: Temperature int
+
+    Return:
+        None
+    """
     def addTemp(self, time : str, temp : int) -> None:
         cursor = self.db.cursor()
         cursor.execute("INSERT INTO temps (time, temp) VALUES ('{}',{});".format(time,temp))
         self.db.commit()
 
+    """
+    Used to delete entries in table.
+
+    Args:
+        None
+
+    Return:
+        None
+    """
     def deleteTemps(self) -> None:
         cursor = self.db.cursor()
         cursor.execute("DELETE FROM temps")
--- temperatures.py
+++ temperatures.py
@@ -1,28 +1,53 @@
-
-
-
-from flask import Flask, jsonify, request, render_template, session
+from flask import Flask, jsonify, request, render_template
 from flask_session import Session
 from datetime import datetime
 from pytz import timezone
 from initDB import myDB
 
+"""
+Initialize app and configure for sessions (to be implemented in future).
+
+This file defines API endpoints for the webserver, mostly POST and GET.
+"""
 app = Flask(__name__)
 app.jinja_env.globals.update(zip=zip) #defines jinja zip function to be same as py zip for HTML use
 app.config.update(SECRET_KEY="MyTotallSecreyKey")
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
-tempDB = myDB()
+"""
+Yeah, globals because sessions don't work nice with ESP32.
 
+TODO: Either pass/return relevant variables or use classes.
+"""
+tempDB = myDB()
 temperatures = []
 times = []
 probeEnabled = False
 
+"""
+Routes to the plot page where line graph is plotted of the data. X axis: datetime, Y axis: temperature
+
+Args:
+    None
+
+Return:
+    None
+"""
 @app.route('/plotTemps')
 def plotPlage() -> None:
     return render_template('plots.html',timeList=times, tempList=temperatures)
 
+
+"""
+Stops the data probe on the ESP32. Simply sets a global variable for the ESP32 to read in.
+
+Args:
+    None
+
+Return:
+    None
+"""
 @app.route('/stopProbe', methods=["POST"])
 def stopProbe() -> None:
     global probeEnabled
@@ -30,6 +55,15 @@ def stopProbe() -> None:
     response = jsonify(response = "Probe disabled.", code = 200)
     return response
 
+"""
+Starts the data probe on the ESP32. Simply sets a global variable for the ESP32 to read in.
+
+Args:
+    None
+
+Return:
+    None
+"""
 @app.route('/initiateProbe', methods=["POST"])
 def initiateProbe() -> None:
     global probeEnabled
@@ -37,11 +71,29 @@ def initiateProbe() -> None:
     response = jsonify(response = "Probe enabled.", code = 200)
     return response
 
-@app.route('/getProbeStatus', methods=["GET"]) #to be used by ESP32 to get probeEnabled status
+"""
+Endpoint used by ESP32 to get probeEnabled status
+
+Args:
+    None
+
+Return:
+    None
+"""
+@app.route('/getProbeStatus', methods=["GET"])
 def getProbeStatus() -> None:
     response = jsonify(response = {"Probe status" : probeEnabled}, code = 200)
     return response
 
+"""
+Clears data from internal lists, as well as the entries in the mySQL table.
+
+Args:
+    None
+
+Return:
+    None
+"""
 @app.route('/clearTemperature', methods=["POST"])
 def clearTemperature() -> None:
     tempDB.deleteTemps()
@@ -50,10 +102,28 @@ def clearTemperature() -> None:
     response = jsonify(response = "Successfully cleared temperature.", code = 200)
     return response
 
+"""
+Renders the landing page.
+
+Args:
+    None
+
+Return:
+    None
+"""
 @app.route("/", methods=["GET"])
 def getTemperature() -> None:
     return render_template('index.html',timeList=times,tempList=temperatures)
 
+"""
+Used by ESP32 to add data to internal lists as well as mySQL table.
+
+Args:
+    None
+
+Return:
+    None
+"""
 @app.route("/", methods=["POST"])
 def addTemperature() -> None:
     tz = timezone('EST')
